3b5d174739cf1d2747e5c1cd631d40939f663093
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-10 14:04:31 +0530

    Day 2: Python data types practice

--- app.py
+++
@@ -1,4 +0,0 @@
-x=3
-y="amit"
-print(type(x))
-print(type(y))
---
+++ data_types.py
@@ -0,0 +1,29 @@
+
+"""
+
+these are the data tupes in pythone 
+Text Type:    	str
+Numeric Types:	 int, float, complex
+Sequence Types:  list, tuple, range
+Mapping Type: 	 dict
+Set Types:    	set,  frozenset
+Boolean Type: 	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	 NoneType
+"""
+
+
+x=5 # int 
+
+x=3.4 # float
+
+x=4j # complex
+x = "hello" #string
+x=[1,2,3,4] # list 
+x=(1,2,3,) # tuple
+x=range(6) # range
+x={1,2,3} #set 
+x=frozenset({1,2,3}) # frozenset
+x=bytearry(b"hello") # byte array
+
+x=bytes(2) # bytes
