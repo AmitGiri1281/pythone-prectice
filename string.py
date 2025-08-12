076f934ad431ca4e19c31ae436a968d3fc47b650
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-12 19:32:09 +0530

    Day-3: learning string concept in pythone

---
+++ string.py
@@ -0,0 +1,89 @@
+# # a="amit"
+# # A='amit'
+
+# # b="""
+# # askjdgkjhdg
+# # wweefjbwljegkkf
+# # webfjwgbi"""
+# # print(b)
+
+# # a="hello, world" # in pythone indexing is start from 0
+
+# # print(a[6])
+
+# for x in "banana":
+#     print(x) # memory view
+#     x=memoryview(bytes(5))
+#     print(x)
+#     x="banan"
+#     print(len(x))#print length of tring 
+
+#     txt="sbdfjgsuifg jbsdjfgs hi dhsi free"
+#     print("free" in txt) # it chek is present free in sentence
+
+
+#     if "frei" in txt:
+#         print("yes, it is present ")
+#     else :
+#         print("no, it is not present")
+
+
+
+
+# Pythone- string slice (retuen range of charcter )
+
+
+# x="hellor , world!"
+# print(x[-2:-5])
+
+
+# modify string ( uper case, lowercase etc)
+
+
+# a="amit"
+# print(a.upper())
+# print(a)
+
+
+# a= " hello world "
+# print(a.strip())
+# print(a.replace("hello", "hi"))
+
+
+
+# string concanate 
+
+# a="amit"
+# b="giri"
+# c=a +b # without space
+# print(c)
+# print(a + " " + b)# add space 
+
+
+# sting formate  bhejio
+#  x= 34
+# y="amit" 
+
+# print(x+y) # it is not combnile 
+
+
+# but we can combine a string and number by using f-string or for
+
+# x=34
+# y=f"amit"
+# print(f"{x} {y}")
+# txt=f"my name is amit, i am {x})"
+# print(txt)
+
+# x= 34
+# txt=f"the price is{x: .2f} ruppes"
+# print(txt)
+
+
+
+# pythone scpe character 
+txt= " my name is amit giri"
+print(txt)
+
+a= "my name is \"amit\" giri"
+print(a)
