d63dd2d34a9dbe09db6415a1832164189ca902aa
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-13 09:30:13 +0530

    day-3: python operator and its peredence

---
+++ operator.py
@@ -0,0 +1,64 @@
+# # #arthimatic operator
+
+# # a=4
+# # b=6
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+# # print(a%b)
+
+# # print(a**b)
+
+# # print(a//b)
+
+
+# x=2
+# x&=3
+# print(x)
+
+# # x>>2
+# # print(x)
+# x<<2
+# print(x)
+
+# # comprision operatrt=
+
+# ==
+# >
+# <
+# !=
+# >=
+# <=
+
+# # logical operator
+# and
+# ornot
+
+
+# # identity opertor
+# x is y
+
+# x is not y
+
+# # membor ship operator (test is sequence is present in objcect)
+# x in y
+#  x not in y
+
+
+# # bitwise operatoor 
+# &
+# | or
+
+# ^ xor
+
+# ~ not
+
+#  << zero fill left shift
+# >> risght 
+
+
+# paerthesis has hieger persidence
+
+
+print(5+4-7+3)
