5586a74244a6bbf9f531c6199c248b9d722e28fc
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-13 09:06:25 +0530

    Day-4: learning boolean and its concept in python

---
+++ bolean.py
@@ -0,0 +1,44 @@
+# true or false
+
+# x= 4
+# if x>4:
+#     print("yes")
+#     print(2>4)
+
+# 
+# print(bool(0))
+# print(bool("amit"))
+
+# x="amit"
+# print(bool(x))
+
+# print(bool("false"))
+# print(bool())
+
+
+# class myclass():
+#   def __len__(self):
+#     return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+
+# def myfunction():
+#     return True
+
+
+
+#  print(myfunction())
+
+# def myfunction():
+#     return True
+
+# if myfunction():
+#     print("yes")
+# else:
+#     print("no")
+
+x = 300
+
+print(isinstance(x, int)) # check data types and give boolean value
