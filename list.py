4ebfd647ce0f3782b458e52acc00cbeaa635e7e8
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-14 08:00:43 +0530

    add sort opertion on list

--- list.py
+++ list.py
@@ -1,75 +1,75 @@
-# # created using []
+# created using []
 
 
-# mylist = ["mango", "apple", "banana"]
-# print(mylist[0])
-# print(len(mylist))
+mylist = ["mango", "apple", "banana"]
+print(mylist[0])
+print(len(mylist))
 
-# testlist = [1 ,"amit", False]
-# print(testlist)
-# print(type(testlist)) 
-# newlist=(("amit", "giri"))
-# print(type(newlist))
-# """
-# List is a collection which is ordered and changeable. Allows duplicate members.
-# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
-# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
-# Dictionary is a collection which is ordered** and changeable. No duplicate members.
-# """
-
-
-# # in pytthon negetive indexing means starting from the end of thie list
-
-# rangelist = ["amit","giri","sachin","rohan","rajesh"]
-# print(rangelist[2:4])
-
-# if "rohan" in rangelist:
-#     print("yes")
+testlist = [1 ,"amit", False]
+print(testlist)
+print(type(testlist)) 
+newlist=(("amit", "giri"))
+print(type(newlist))
+"""
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+"""
 
 
-# print(rangelist[-2:])
+# in pytthon negetive indexing means starting from the end of thie list
+
+rangelist = ["amit","giri","sachin","rohan","rajesh"]
+print(rangelist[2:4])
+
+if "rohan" in rangelist:
+    print("yes")
+
+
+print(rangelist[-2:])
 
 
 
-# rangelist.insert(3, "balck")
-# rangelist[1]="red"
-# print(rangelist)
+rangelist.insert(3, "balck")
+rangelist[1]="red"
+print(rangelist)
 
 
-# newlist = ["amit", "giti"]
-# newlist.append("mata")
-# print(newlist)
+newlist = ["amit", "giti"]
+newlist.append("mata")
+print(newlist)
 
-# oldlist = ["rahul" "giri"]
-# newlist.extend(oldlist)
-# print(newlist)
-# newlist.append(oldlist)
-# print(newlist)
-
-
-# newlist.insert(1, "balck")
-# print(newlist)
+oldlist = ["rahul" "giri"]
+newlist.extend(oldlist)
+print(newlist)
+newlist.append(oldlist)
+print(newlist)
 
 
-# newlist.remove("giti")
-# print(newlist)
-
-# newlist.pop(1)
-# print(newlist)
+newlist.insert(1, "balck")
+print(newlist)
 
 
-# list = ["amit", "giri" , "sacchin"]
+newlist.remove("giti")
+print(newlist)
 
-# for w in list:
-#     print(w)
+newlist.pop(1)
+print(newlist)
 
-# for i in range(len(list)):
-#     print(list[i])
 
-# i=0
-# while i<len(list):
-#     print(list[i])
-#     i =i+1
+list = ["amit", "giri" , "sacchin"]
+
+for w in list:
+    print(w)
+
+for i in range(len(list)):
+    print(list[i])
+
+i=0
+while i<len(list):
+    print(list[i])
+    i =i+1
 
 
 newlist = ["amit", "giri", "mataji",  "papaji"]
@@ -80,3 +80,17 @@ for i in newlist:
     if "a" in newlist:
         checklist.append(i)
         print(checklist)
+
+newlist.sort()
+print(newlist)
+numberlist = [45, 12, 32, 2 ,34,44]
+
+numberlist.sort(reverse=False)
+print(numberlist)
+numberlist.sort()
+
+
+
+print(numberlist)
+numberlist.reverse()
+print(numberlist)
