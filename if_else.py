000e4aac082a657f5ddbd7455a9fceaf30b58c2f
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-17 00:01:44 +0530

    Day-7: learing if else statemnt oin python

---
+++ if_else.py
@@ -0,0 +1,30 @@
+# a=3
+# b=5
+# if a>b:
+#     print("a is geater than b")
+# elif a==b:
+#    print("a is equale to b")
+# else :
+#  print("no")
+
+
+# a=2
+# b=3
+# c=4
+# if a>b & b>c:
+#     print("A is greater number")
+# else:
+#     print("a is not greate number ")
+
+
+
+
+# nested query / loop
+
+a =3
+b=3 
+c=6
+if c>a:\
+print("c is greater")
+if a==b:
+ print("c is greate uumber")
