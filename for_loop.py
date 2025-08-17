8ac5e47224a32759e54cc30282777783afa657bc
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-17 11:19:35 +0530

    Day-7: for_loop

---
+++ for_loop.py
@@ -0,0 +1,20 @@
+fruites =["apple" , "banana", "mango", "papaya"]
+for x in fruites:
+    print(x)
+
+
+for  x in "banana":
+    print(x) # it give all bana devide in arr
+
+
+for x in fruites:
+    print(x)
+    if x =="mango":
+        break
+    print(x)
+
+for x in range(2,4):
+    print(x)
+
+for x in [0,1,2]:
+    pass
