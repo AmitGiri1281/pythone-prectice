d556f050d3ec9567b9e8336d5fab0f04d1ba65f4
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-17 10:59:15 +0530

    Day-7: while statemet (loop)

---
+++ while.py
@@ -0,0 +1,27 @@
+i =1
+
+while i < 6:
+ print("hellow world!")
+i = i+1
+
+i = 1
+while i < 6:
+  print("hello world")
+  i += 1
+
+a =0
+
+while a < 6:
+    a += 1
+    if a == 3:
+        continue
+    print(a)
+
+
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+else :
+    print("i is no longer less than 5")
