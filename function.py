84a9e5d847a38e79d9dfe659d07bd338bc2de7ae
Amit Giri <116455388+AmitGiri1281@users.noreply.github.com>
2025-08-18 23:55:36 +0530

    Day-8:learning python function

---
+++ function.py
@@ -0,0 +1,19 @@
+# def keyord is used to deifne a function
+
+def myfunction():
+    print("Hello world")
+
+myfunction() # function call
+
+
+
+
+# arment and parmeter bothe are same which i sgiven in the finction 
+
+
+def newfun(fname, lname)
+    print(fname, lname)
+
+
+
+newfun("amit", "giri")
